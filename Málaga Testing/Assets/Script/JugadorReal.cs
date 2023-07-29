10ebfda7d1f8a15ff0a40cd9656730b2a29bd478
Psydoy21 <fernandezlucasjaime@gmail.com>
2023-07-29 22:28:27 +0200

    Cosas quue me da pereza describir

--- Málaga Testing/Assets/Script/JugadorReal.cs
+++ Málaga Testing/Assets/Script/JugadorReal.cs
@@ -8,6 +8,7 @@ public class JugadorReal : MonoBehaviour
     [HideInInspector]  Rigidbody2D rb;
     [SerializeField]  LayerMask suelo;
     [SerializeField] GameObject objeto;
+    [SerializeField] SpriteRenderer spriteR;
 
     [HideInInspector] private float speed;
     [SerializeField] private float aceleration;
@@ -30,6 +31,7 @@ public class JugadorReal : MonoBehaviour
     }
     void Update()
     {
+        flipx();
         movimientoHorizontal();
         if(Mathf.Abs(move)>0)
         {
