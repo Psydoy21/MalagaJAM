6e013651a15ec9ad2b5dc31d18d0c10ec26e17ff
Javiowo03 <jmp3498@gmail.com>
2023-07-29 11:54:06 +0200

    Real1 avances
    el texto furula

---
+++ Málaga Testing/Assets/Script/JugadorReal.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JugadorReal : MonoBehaviour
+{
+   
+    [HideInInspector]  Rigidbody2D rb;
+    [SerializeField]  LayerMask suelo;
+    [SerializeField] GameObject objeto;
+
+    [HideInInspector] private float speed;
+    [SerializeField] private float aceleration;
+    [SerializeField] private float deceleration;
+    [SerializeField] private float maxVelocity;
+    [HideInInspector] public float move;
+
+
+    private float tiempoEnElAire;
+    [SerializeField] private float AlturaMax;
+    private float numeroSaltos;
+    [SerializeField] private float velocidadSalto;
+
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+    }
+    void Update()
+    {
+        movimientoHorizontal();
+    }
+
+    private void movimientoHorizontal()
+    {
+        move = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(speed, rb.velocity.y);
+        if (move == 0)
+        {
+            if (Mathf.Abs(speed) <= 0.1f)
+            {
+                speed = 0;
+            }
+            else
+            {
+                if (speed > 0)
+                {
+                    speed = speed - deceleration * Time.deltaTime;
+                }
+                else
+                {
+                    speed = speed + deceleration * Time.deltaTime;
+                }
+            }
+        }
+        else
+        {
+            if (move * speed < 0)
+            {
+                speed = speed + deceleration * Time.deltaTime * move;
+            }
+            else
+            {
+                if (Mathf.Abs(speed) >= maxVelocity)
+                {
+                    speed = maxVelocity * move;
+                }
+                else
+                {
+                    speed = speed + aceleration * Time.deltaTime * move;
+                }
+            }
+        }
+    }
+    private void movimientoVertical()
+    {
+        RaycastHit2D raycastSueloL = Physics2D.Raycast((transform.position + Vector3.down * 0.5f + Vector3.left * 0.65f), Vector2.down, 1f, suelo);
+        RaycastHit2D raycastSueloR = Physics2D.Raycast((transform.position + Vector3.down * 0.5f + Vector3.right * 0.5f), Vector2.down, 1f, suelo);
+        Debug.DrawRay((transform.position + Vector3.left * 0.65f + Vector3.down * 0.5f), Vector3.down * 1f, Color.green);
+        Debug.DrawRay((transform.position + Vector3.right * 0.5f + Vector3.down * 0.5f), Vector3.down * 1f, Color.green);
+
+
+        if (Input.GetKey(KeyCode.Space) && tiempoEnElAire < AlturaMax && numeroSaltos < 1)
+        {
+            tiempoEnElAire = tiempoEnElAire + Time.deltaTime;
+            rb.velocity = new Vector2(rb.velocity.x, velocidadSalto);
+
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            numeroSaltos = numeroSaltos + 1f;
+        }
+
+
+
+        if ((raycastSueloL == false) && (raycastSueloR == false) && Input.GetKey(KeyCode.Space) == false)
+        {
+            numeroSaltos = numeroSaltos + 6f * Time.deltaTime;
+        }
+
+        if (raycastSueloL == true || raycastSueloR == true)
+        {
+            tiempoEnElAire = 0f;
+            numeroSaltos = 0f;
+        }
+
+
+    }
+    public void interactuar()
+    {
+        if ( Input.GetKeyDown(KeyCode.E) == true)
+        {
+            objeto.SetActive(true);
+        }
+    }
+
+
+}
--- Málaga Testing/Assets/objeto.cs
+++ Málaga Testing/Assets/objeto.cs
@@ -1,18 +1,105 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Playables;
+using UnityEngine.UI;
 
 public class objeto : MonoBehaviour
 {
+    [SerializeField] public GameObject panel;
+    [SerializeField] public Text texto;
+    public bool inrange;
+    public bool objetoc;
+    private bool isopen;
+    [SerializeField] public GameObject exclamacion;
+    [SerializeField] public int linea;
+    public int longi;
+    [SerializeField, TextArea(4, 6)] private string[] lineas;
+    public float type;
+    public TextAsset textito;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(textito!=null)
+        {
+            lineas = (textito.text.Split("\n"));
+        }
+        longi = lineas.Length;
+        inrange = false;
+        panel.SetActive(false);
+        exclamacion.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(inrange&&!isopen) {
+                StartDialogue();
+            }
+            else if(isopen&& texto.text == lineas[linea])
+            {
+                closedialogue();
+            }
+            else
+            {
+                completedialogue();
+            }
+        }
+    }
+    private void completedialogue()
+    {
+        //si añado otra corutina se va a liar
+        StopAllCoroutines();
+        texto.text = lineas[linea];
+    }
+    private void closedialogue()
+    {
+        panel.SetActive(false);
+        exclamacion.SetActive(true);
+        Time.timeScale = 1f;
+        isopen = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            exclamacion.SetActive(true);
+            inrange = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            exclamacion.SetActive(false);
+            inrange = false;
+        }
+    }
+    private void StartDialogue()
+    {
+        isopen = true;
+        panel.SetActive(true);
+        exclamacion.SetActive(false);
+        linea = 0;
+        Time.timeScale = 0f;
+        StartCoroutine(Showline());
+
+    }
+
+    private IEnumerator Showline()
+    {
+        int charindex;
+        charindex = 0;
+        texto.text = string.Empty;
+        foreach (char ch in lineas[linea])
+        {
+            texto.text += ch;
+            charindex++;
+            yield return new WaitForSecondsRealtime(type);
+        }
     }
 }
