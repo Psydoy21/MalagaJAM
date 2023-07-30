0605d9a6d83a8843ab6bc531af3a25ba053099c7
Psydoy21 <fernandezlucasjaime@gmail.com>
2023-07-30 09:39:35 +0200

    Fade in/out real 4

--- Málaga Testing/Assets/Script/triggerCoche.cs
+++ Málaga Testing/Assets/Script/triggerCoche.cs
@@ -6,16 +6,27 @@ using UnityEngine.SceneManagement;
 public class triggerCoche : MonoBehaviour
 {
     int nextSceneIndex;
-    
-    
+    [SerializeField] GameObject fade;
+    Animator animator;
+    private void Start()
+    {
+        animator = fade.GetComponent<Animator>(); 
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         
-        if (collision.gameObject.tag == "Player") ;
+        if (collision.gameObject.tag == "Player") 
         {
             Debug.Log("Trigger");
-            nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
-            SceneManager.LoadScene(nextSceneIndex);
+            StartCoroutine(SceneLoad());
         }
+    }
+    public IEnumerator SceneLoad()
+    {
+        animator.SetTrigger("Start Transition");
+        yield return new WaitForSeconds(1f);
+        nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        SceneManager.LoadScene(nextSceneIndex);
     }
 }
